4f78f05229b1c7cdd48fd92744c23610a8e00870
Taj <78543798+tajciglar@users.noreply.github.com>
2024-01-11 21:04:43 +0000

    Added ship function and gameboard class

---
+++ script.js
@@ -0,0 +1,51 @@
+export {Ship, Gameboard, Player};
+
+//  Create a battleship
+function Ship(length, hits = 0, sunken = false){
+    return {
+        length: length,
+        hits: hits,
+        sunken: sunken,
+        hit(){
+             this.hits += 1;
+             this.isSunk();
+        },
+        isSunk(){
+            if(this.hits === this.length){
+                this.sunken = true;
+            }
+        },
+    };
+}
+
+// Create a battlefield
+class Gameboard {
+    constructor(){
+        this.board = Array.from({length: 10}, () => Array(10).fill(0));
+        this.ships = [];
+    }
+
+    placeShip(ship, x, y, isHorizantal){
+        this.ships.push(ship);
+        if(isHorizantal === true){
+            for(let i = 0; i < ship.length; i++){
+                this.board[x][y] = 1;
+                y += 1;
+            }
+        } else {
+            for(let i = 0; i < ship.length; i++){
+                this.board[x][y] = 1;
+                x += 1;
+            }
+        }
+    }
+
+    recieveAttack(){
+        
+    }
+}
+
+// Create a player
+function Player(){
+    
+}
---
+++ test.js
@@ -0,0 +1,28 @@
+import { Ship, Gameboard, Player } from "./script";
+
+test('Ship lenght', () => {
+    const ship = Ship(5);
+    expect(ship.length).toBe(5);
+});
+
+test('Ship is hit', () => {
+    const ship = Ship(5);
+    ship.hit();
+    expect(ship.hits).toBe(1);
+});
+
+test('Ship is sunk', () => {
+    const ship = Ship(2);
+    ship.hit();
+    ship.hit();
+    expect(ship.sunken).toBe(true);
+})
+
+test('Ship is placed', () => {
+    const ship1 = Ship(3);
+    const ship2 = Ship(5);
+    const gameboard1 = new Gameboard();
+    gameboard1.placeShip(ship1, 0, 1, true)
+    gameboard1.placeShip(ship2, 2, 2, false)
+    console.log(gameboard1);
+})
