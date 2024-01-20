8bea3428e54f4c6e003dbf312a97d26942e661cf
Taj <78543798+tajciglar@users.noreply.github.com>
2024-01-20 13:07:30 +0000

    Added createShips and placeShips functions

--- src/script.js
+++ src/script.js
@@ -19,8 +19,8 @@ playButton.addEventListener("click", () =>{
     gameboardContainer.setAttribute('id', 'gameboardContainer');
     main.appendChild(gameboardContainer);
 
-    createGameboard(playerGameboard.board, 1);
-    createGameboard(computerGameboard.board, 2);
+    createGameboard(playerGameboard, 1);
+    createGameboard(computerGameboard, 2);
 });
 
 function createGameboard(gameboard, number) {
@@ -30,35 +30,87 @@ function createGameboard(gameboard, number) {
     gameboardDiv.setAttribute('class', 'gameboard');
     gameboardContainer.appendChild(gameboardDiv);
 
-   
+   let ships = [];
+
     if (number === 1) {
         gameboardDiv.classList.add('gameboard1');
         const middleBoard = document.createElement('div');
         middleBoard.setAttribute('id', 'middleBoard');
         gameboardContainer.appendChild(middleBoard);
+        ships = createShips();
     } else if (number === 2) {
-        
         gameboardDiv.classList.add('gameboard2');
     }
-
-    
     
     let rowNumber = 0;
-    gameboard.forEach((array) => {
+    gameboard.board.forEach((array) => {
        
         const newRow = document.createElement('div');
-        newRow.setAttribute('id', rowNumber);
+        newRow.setAttribute('value', rowNumber);
         newRow.setAttribute('class', 'row');
         rowNumber += 1;
         gameboardDiv.appendChild(newRow);
 
         let blockNumber = 0;
-        array.forEach((block) => {
+        array.forEach(() => {
             const newBlock = document.createElement('div');
-            newBlock.setAttribute('id', blockNumber);
+            newBlock.setAttribute('value', blockNumber);
             newBlock.setAttribute('class', 'block');
             blockNumber += 1;
             newRow.appendChild(newBlock);
+            if(number === 1){
+                newBlock.addEventListener('click', () =>{
+                    placeShips(newBlock, gameboard, ships);
+                });
+            }
         });
     });
+
+    if(number === 2){
+        
+        placeShips();
+    }
+}
+
+function createShips(){
+
+    const shipLengths = [1, 2, 3, 4, 5];
+    const ships = shipLengths.map(length => Ship(length));
+
+    return ships;
+}
+
+
+function placeShips(block, gameboard, ships) {
+    console.log(ships);
+    const instructions = document.getElementById('instructions');
+    
+    if (instructions.childNodes.length <= 2) {
+        const message = document.createElement('p');
+        message.textContent = "Place ships on the gameboard";
+        instructions.appendChild(message);
+    }
+
+    /*
+        const messageVertical = document.createElement('div');
+        messageVertical.textContent = "Click here to turn boat vertical";
+        const checkbox = document.createElement('input')
+         checkbox.setAttribute('type', 'checkbox');
+    */ 
+    
+   
+    const x = block.getAttribute('value');
+    const y = block.parentNode.getAttribute('value');
+
+    const shipSize = ships[0].length;
+
+    for (let i = 0; i < shipSize; i++) {
+       
+        const currentBlock = block.parentNode.querySelector(`[value="${parseInt(x) + i}"]`);
+        currentBlock.style.cssText = 'background-color: blue;';
+    }
+
+    gameboard.placeShip(ships[0], x, y, true);
+
+    ships.shift();
 }
